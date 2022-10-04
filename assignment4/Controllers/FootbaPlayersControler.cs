9c80524c9ab2d937c4d98f33cf753ee921477810
F1-Guy <bartol.cagalj@gmail.com>
2022-10-04 13:39:48 +0200

    Initial commit

---
+++ assignment4/Controllers/FootbaPlayersControler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace assignment4.Controllers
+{
+    [Route("api/Players")]
+    [ApiController]
+    public class FootbaPlayersControler : ControllerBase
+    {
+        // GET: api/<Players>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<Players>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Players>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Players>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Players>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
